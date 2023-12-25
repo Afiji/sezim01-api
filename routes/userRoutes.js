0ef67429500927063658d451f532d0ce7d64b763
Sezim Erkinbekova <uivmat3@gmail.com>
2023-12-25 16:31:26 +0600

    little bugs fixed

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -36,26 +36,20 @@ router.post(
     try {
       const userId = req.user.userId;
 
-      // Найти существующего пользователя и удалить старый аватар из S3, если он есть
       const user = await Auth.findById(userId);
       if (user && user.avatar) {
-        // Получаем ключ файла из URL аватара
         const fileKey = user.avatar.split("/").pop();
 
-        // Параметры для удаления файла из S3
         const deleteParams = {
           Bucket: process.env.AWS_BUCKET_NAME,
           Key: `profile-images/${fileKey}`,
         };
 
-        // Отправляем команду на удаление в S3
         await s3Client.send(new DeleteObjectCommand(deleteParams));
       }
 
-      // Загрузить новый аватар
-      const avatarUrl = req.file.location; // URL нового файла на S3
+      const avatarUrl = req.file.location;
 
-      // Обновить пользователя в базе данных с новым URL аватара
       await Auth.findByIdAndUpdate(userId, { avatar: avatarUrl });
       res.status(200).json({
         message: "Avatar uploaded successfully",
@@ -77,8 +71,7 @@ router.delete("/delete-avatar", verifyToken, async (req, res) => {
       return res.status(404).json({ message: "User or avatar not found" });
     }
 
-    // Получаем ключ файла из URL аватара
-    const fileKey = user.avatar.split("profile-images/")[1]; // Исправлено для получения части пути после 'profile-images/'
+    const fileKey = user.avatar.split("profile-images/")[1];
     const deleteParams = {
       Bucket: process.env.AWS_BUCKET_NAME,
       Key: `profile-images/${fileKey}`,
@@ -86,7 +79,6 @@ router.delete("/delete-avatar", verifyToken, async (req, res) => {
 
     await s3Client.send(new DeleteObjectCommand(deleteParams));
 
-    // Обновляем запись пользователя в базе данных, удаляем ссылку на аватар
     await Auth.findByIdAndUpdate(userId, { avatar: "" });
     res.status(200).json({ message: "Avatar deleted successfully" });
   } catch (error) {
